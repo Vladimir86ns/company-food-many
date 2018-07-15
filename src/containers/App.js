9db51a10b8cd113f0162fb8be95323e00335966e
vladimir86ns <vladimir86ns@gmail.com>
2018-07-15 10:45:33 +0200

    added category

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,16 +7,16 @@ class App extends Component {
 
   state = {
     manyItems: [
-      {id: 1, name: 'Kapricoza', price: 250, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 2, name: 'Madjarica', price: 350, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 3, name: 'Domaca', price: 400, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 4, name: 'Sa sirom', price: 100, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 5, name: 'Vegeterijanka', price: 250, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 6, name: 'Specijalitet', price: 300, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 7, name: 'Slatka', price: 250, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 8, name: 'Extra cheese', price: 700, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 9, name: 'Best of house', price: 250, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
-      {id: 10, name: 'Sa lubenicom', price: 200, picture:"http://zoebingleypullin.com/wp-content/uploads/sites/4/2017/05/500x500xMung-bean-500x500.jpg.pagespeed.ic.jOSsRJ0PQ5.jpg"},
+      {id: 1, name: 'Kapricoza', price: 250, category: 'pizza', picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 2, name: 'Madjarica', price: 350, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 3, name: 'Domaca', price: 400, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 4, name: 'Sa sirom', price: 100, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 5, name: 'Vegeterijanka', price: 250, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 6, name: 'Specijalitet', price: 300, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 7, name: 'Slatka', price: 250, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 8, name: 'Extra cheese', price: 700, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 9, name: 'Best of house', price: 250, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
+      {id: 10, name: 'Sa lubenicom', price: 200, category: 'pizza',  picture:"https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/Hawaiiane0e765.jpg"},
     ]
   }
 
