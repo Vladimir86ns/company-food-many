03ae5d178476921fe0177e256e9fb71f20f6e069
vladimir86ns <vladimir86ns@gmail.com>
2018-09-09 12:14:28 +0200

    refactored/ Checking order page

--- src/containers/CheckingOrder/Checking-order.js
+++ src/containers/CheckingOrder/Checking-order.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { checkUser, getCompanyId } from '../../store/utility';
 import * as actionTypes from './index';
 import './Checking-order.css';
 import Aux from '../../hoc/Aux';
@@ -15,20 +16,12 @@ class CheckingOrder extends Component {
    * Fetch all orders
    */
   componentDidMount() {
-    if(this.checkUser()) {
+    if(checkUser()) {
       this.props.history.push("login")
     };
 
-    let companyId = localStorage.getItem('company_id');
+    let companyId = getCompanyId();
     this.props.initOrders(companyId);
-  }
-
-  /**
-   * Check user is logged
-   */
-  checkUser() {
-    let token = localStorage.getItem('jwt');
-    return token === null;
   }
 
   /**
@@ -59,76 +52,129 @@ class CheckingOrder extends Component {
     });
   }
 
+  /**
+   * Get all orders in progress.
+   */
+  getAllOrders() {
+    return this.props.allOrders.map((item, index) => {
+      let countItem = JSON.parse(item.order_items).length;
+        return (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{item.created_at}</td>
+            <td>{countItem}</td>
+            <td><button onClick={() => this.orderDetail(item)}>Details</button></td>
+          </tr>
+        )
+    });
+  }
+
+  /**
+   * Get table list orders.
+   */
+  getListOrdersTable(orders) {
+    return (
+      <div>
+        <button onClick={this.refreshPage}>REFRESH</button>
+        <table>
+          <tbody>
+            <tr>
+              <th>#</th>
+              <th>Order time</th>
+              <th>Sum items</th>
+              <th>Check Order</th>
+            </tr>
+            {orders}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  /**
+   * Get all details from one order.
+   */
+  getOrderDetails() {
+    let items = JSON.parse(this.state.orderDetail.order_items);
+
+    let allOrderDetails = items.map((item, index) => {
+      // Toggle button if order is in progress or done
+      let button;
+
+      // if order is done display Done button
+      if (this.state.finishOrderIndex.includes(index)) {
+        button = (<td><button >Done</button></td>)
+      } else {
+        button = (<td><button style={{backgroundColor: 'red'}} onClick={() => this.finishSingleItem(index)}>Progress</button></td>)
+      }
+
+      return (
+        <tr key={index}>
+        <td>{index + 1}</td>
+        <td>{item[0]}</td>
+        <td>{item[1]}</td>
+        {button}
+      </tr>
+      );
+    })
+
+    // display all order items
+    return this.getListOrderDetailsTable(allOrderDetails, items);
+  }
+
+  /**
+   * Get table list order details.
+   */
+  getListOrderDetailsTable(allOrderDetails, items) {
+    // Initialize close button
+    let orderButtonClose = this.checkCloseButton(items);
+
+    return (
+      <div>
+        <button onClick={this.refreshPage}>BACK TO ORDERS</button>
+        <table>
+          <tbody>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Size</th>
+              <th>Check Order</th>
+            </tr>
+            {allOrderDetails}
+          </tbody>
+        </table>
+        {orderButtonClose}
+      </div>
+    );
+  }
+
+  /**
+   * If all items are done, display close button, otherwise,
+   * items are not finished.
+   */
+  checkCloseButton(items) {
+    // if all items are done, display close button
+    if (this.state.finishOrderIndex.length === items.length) {
+      return (<button onClick={() => this.props.closeOrder(this.state.orderDetail.id)}>CLOSE ORDER</button>);
+    }
+
+    return (<button style={{backgroundColor : "red"}} >ITEMS ARE NOT FINISHED!</button>);
+  }
+
   render() {
     // Initialize single order details
     let orderDetail = (<div></div>);
 
-    // Initialize close button
-    let orderButtonClose = (<button style={{backgroundColor : "red"}} >ITEMS ARE NOT FINISHED!</button>);
-
     // Display order details if is clicked on button
     if (this.state.orderDetail) {
-      let items = JSON.parse(this.state.orderDetail.order_items);
-      let allOrderDetails = items.map((item, index) => {
-        // Toggle button if order is in progress or done
-        let button;
-
-        // if order is done display Done button
-        if (this.state.finishOrderIndex.includes(index)) {
-          button = (<td><button >Done</button></td>)
-        } else {
-          button = (<td><button style={{backgroundColor: 'red'}} onClick={() => this.finishSingleItem(index)}>Progress</button></td>)
-        }
-
-        return (
-          <tr key={index}>
-          <td>{index + 1}</td>
-          <td>{item[0]}</td>
-          <td>{item[1]}</td>
-          {button}
-        </tr>
-        );
-      })
-
-      // if all items are done, display close button
-      if (this.state.finishOrderIndex.length === items.length) {
-        orderButtonClose = (<button onClick={() => this.props.closeOrder(this.state.orderDetail.id)}>CLOSE ORDER</button>);
-      }
-
-      // display all order items
-      orderDetail = (
-        <div>
-          <button onClick={this.refreshPage}>BACK TO ORDERS</button>
-          <table>
-            <tbody>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Size</th>
-                <th>Check Order</th>
-              </tr>
-              {allOrderDetails}
-            </tbody>
-          </table>
-          {orderButtonClose}
-        </div>
-      );
+      orderDetail = this.getOrderDetails();
     }
 
-    // Check fetching orders, and display if is more then 0.
     let orders = (<tr><td></td></tr>);
+
+    // Check fetching orders, and display if is more then 0.
     if (this.props.allOrders.length > 0) {
-       orders = this.props.allOrders.map((item, index) => {
-        let countItem = JSON.parse(item.order_items).length;
-          return (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{item.created_at}</td>
-              <td>{countItem}</td>
-              <td><button onClick={() => this.orderDetail(item)}>Details</button></td>
-            </tr>
-          )
-      });
+       orders = this.getAllOrders();
     }
 
     // Initialize list of order
@@ -136,22 +182,7 @@ class CheckingOrder extends Component {
 
     // If order details is empty, show list of all orders
     if (!this.state.orderDetail) {
-      listOrders = (
-        <div>
-          <button onClick={this.refreshPage}>REFRESH</button>
-          <table>
-            <tbody>
-              <tr>
-                <th>#</th>
-                <th>Order time</th>
-                <th>Sum items</th>
-                <th>Check Order</th>
-              </tr>
-              {orders}
-            </tbody>
-          </table>
-        </div>
-      );
+      listOrders = this.getListOrdersTable(orders);
     }
 
     return (
--- src/store/utility.js
+++ src/store/utility.js
@@ -1,6 +1,26 @@
+
+/**
+ * Clone old state, and update wit new state.
+ */
 export const updateObject = (oldObject, updatedValues) => {
   return {
     ...oldObject,
     ...updatedValues
   }
 };
+
+/**
+ * Return boolean, if user token is valid.
+ */
+export const checkUser = () => {
+  let token = localStorage.getItem('jwt');
+
+  return token === null;
+}
+
+/**
+ * Return company id from local storage.
+ */
+export const getCompanyId = () => {
+  return localStorage.getItem('company_id');
+}
