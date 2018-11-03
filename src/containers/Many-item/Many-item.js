3cd331b1d9626b60dac67b599b02db9573ebc251
vladimir86ns <vladimir86ns@gmail.com>
2018-11-03 13:55:44 +0100

    improvement - gix main many component

--- src/containers/Many-item/Many-item.js
+++ src/containers/Many-item/Many-item.js
@@ -7,12 +7,7 @@ import *  as actionTypes from '../../store/order/actions';
 import { getCompanyProductImagePath } from '../../utils';
 
 class ManyItem extends Component {
-
-  componentDidMount() {
-    console.log('componentDidMount()');
-  }
-
- render() {
+  render() {
     let showNameWithImage = (
       <Aux>
         <img
